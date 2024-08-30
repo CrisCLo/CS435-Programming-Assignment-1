44b6e4717eecdfc85fb829d360d1501a947ad269
CrisCLo <95257381+CrisCLo@users.noreply.github.com>
2024-08-30 17:14:06 -0400

    Debugging progress

--- GUILeafHighlighter.py
+++ GUILeafHighlighter.py
@@ -4,18 +4,21 @@ import re
 import os
 
 def retrieve_bounds(line):
+    print("In retrieve bounds")
     # Simple regex to recognize and retrieve bounds based on pattern
-    bounds = re.search(r'bounds="(\[(\d)+,(\d)+\]\[(\d)+,(\d)+\]"',line)
+    bounds = re.search(r'bounds="\[(\d)+,(\d)+\]\[(\d)+,(\d)+\]"',line)
     #Once retrieved, we must get each individual coordinate from the capturing groups and convert
     # them to integers within a tuple for drawing later
-    north = int(bounds.groupd(2))
-    south = int(bounds.groups(4))
-    east = int(bounds.groups(3))
-    west = int(bounds.groups(1))
-    return (west,north,east,south)
-
+    if bounds:
+        north = int(bounds.group(2))
+        south = int(bounds.group(4))
+        east = int(bounds.group(3))
+        west = int(bounds.group(1))
+        return (west,north,east,south)
+    return None
 
 def retrieve_leafs(xmlfilename):
+    print('In retrieve leafs')
     #Initialize an array to store the leaf bounds
     leafs = []
     # Initialize a stack to track open tags to match with corresponding closing tags to
@@ -23,9 +26,9 @@ def retrieve_leafs(xmlfilename):
     stack = []
 
     #Open XML File for reading
-    with open(xmlfilename,'r',encoding = 'UTF-8'):
+    with open(xmlfilename,'r',encoding = 'UTF-8') as file:
         #Read file line by line to make it easier to parse
-        lines = xmlfilename.readlines()
+        lines = file.readlines()
 
         for num, line in enumerate(lines):
             # Utilize simple regex to identify the tags when the lines are scanned 
@@ -38,8 +41,8 @@ def retrieve_leafs(xmlfilename):
             # and look for matching non-self closing tag, we do not add self closing tags to the stack
 
             if opentag:
-                #Add the line number to the stack to help with retrieving bounds
-                stack.append(num)
+                #Add the tag name to the stack to help with retrieving bounds
+                stack.append((opentag.group(1),num))
 
             elif selfclosetag:
                 # If it is a self closed tag, extract the bounds immediately as it is 
@@ -53,7 +56,7 @@ def retrieve_leafs(xmlfilename):
                 # if it does match, it is a leaf
                 # pop and retrieve the bounds utilizing the line number from the opening tag
                 if stack and stack[-1][0] == closedtag.group(1):
-                    openlinenum = stack.pop()
+                    tagname,openlinenum = stack.pop()
                     # if the opening tag number is one line before the closing tag, it is the
                     # corresponding closing tag so we retrieve the bounds of the leaf
                     if openlinenum == num - 1:
@@ -62,6 +65,7 @@ def retrieve_leafs(xmlfilename):
     return leafs
 
 def highlightLeafs(image,coords,outputDirectory = 'GeneratedPNGs'):
+    print('InhighlightLeafs')
     # First retrieve image file and open it with PIL library then create a Draw object to draw 
     # boxes on the existing images
     png = Image.open(image)
@@ -72,7 +76,7 @@ def highlightLeafs(image,coords,outputDirectory = 'GeneratedPNGs'):
         highlight.rectangle(boundset,outline = "yellow",width = 3)
 
     # Save the highlighted image as a new image in the GeneratedPNGs folder
-    nonhighlightedname = os.path.splitext(image)[0]
+    nonhighlightedname = os.path.splitext(os.path.basename(image))[0]
     highlightednamepath = os.path.join(outputDirectory,f"{nonhighlightedname}_highlighted.png")
 
     png.save(highlightednamepath)
@@ -82,17 +86,18 @@ def highlightLeafs(image,coords,outputDirectory = 'GeneratedPNGs'):
 
 
 
-def MatchandHighlight(directory = '\Programming-Assignment-Data\Programming-Assignment-Data'):
+def MatchandHighlight(directory = 'Programming-Assignment-Data\Programming-Assignment-Data'):
+    print('In MatchandHighlight')
     #First, Retrieve all files ending in .xml files from input folder and put them in a list
     xmls = glob.glob(os.path.join(directory,"*.xml"))
-
+    #print(xmls)
     #For each xml, determine if it's pair exists within the directory and notify the user
     # if it does not
     for xml in xmls:
         # Get file name with no extension
         basefilename = os.path.splitext(xml)[0]
-        pngmatchname = os.path.join(directory,f"{basefilename}.png")
-
+        pngmatchname = os.path.join(f"{basefilename}.png")
+        #print(pngmatchname)
         #Verify that matching PNG file exists within directory and if it does
         # extract the bounds of the leaf components with other nested functions
         if os.path.exists(pngmatchname):
